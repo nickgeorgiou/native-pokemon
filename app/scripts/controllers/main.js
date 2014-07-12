83efded0744771b03320662bb921b50c94ca8479
Nick Georgiou <nick@Nicks-MacBook-Air.local>
2014-07-12 19:37:30 +1000

    Bespoke Animal Class Attacks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,89 +8,60 @@ angular.module('nickApp')
             'Karma'
         ];
 
-        $scope.animals = [{
-            id: 1,
-            name: 'Monkey Bear',
-            hp: 84,
-            maxHp: 84,
-            attacks: [{
-                attackPower: 11,
-                accuracy: 0.9,
-                name: 'Tackle',
+        $scope.animalClassAttacks = {
+            'Amphibia': [{
+                attackPower: 10,
+                accuracy: 0.95,
+                name: 'Croak',
+                simplePast: 'croaked at',
+                pastParticiple: 'croaked at'
+            }, {
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackled',
                 simplePast: 'tackled',
                 pastParticiple: 'tackled'
+            }],
+            'Actinopterygii': [{
+                attackPower: 10,
+                accuracy: 0.95,
+                name: 'Splash',
+                simplePast: 'splashed toward',
+                pastParticiple: 'splashed at'
             }, {
-                attackPower: 16,
-                accuracy: 0.6,
-                name: 'Bite',
-                simplePast: 'bit',
-                pastParticiple: 'bitten'
-            }]
-        }, {
-            id: 2,
-            name: 'Cat',
-            hp: 91,
-            maxHp: 91,
-            attackPower: '20',
-            attacks: [{
                 attackPower: 20,
-                accuracy: 0.7,
-                name: 'Tackle',
-                simplePast: 'tackled',
-                pastParticiple: 'tacked'
-            }, {
-                attackPower: 40,
-                accuracy: 0.2,
-                name: 'Scratch',
-                simplePast: 'scratched',
-                pastParticiple: 'scratched'
+                accuracy: 0.6,
+                name: 'Fin Spike',
+                simplePast: 'used fin spike on',
+                pastParticiple: 'fin spiked'
             }]
-        }];
+        };
 
-        $scope.wildAnimals = [{
-            id: 3,
-            name: 'Fox',
-            hp: 83,
-            maxHp: 83,
-            attacks: [{
-                attackPower: 10,
-                accuracy: 1,
-                name: 'Scratch',
-                simplePast: 'scratched',
-                pastParticiple: 'scratched'
-            }, {
-                attackPower: 20,
-                accuracy: 0.6,
-                name: 'Bite',
-                simplePast: 'bit',
-                pastParticiple: 'bitten'
-            }]
-        }, {
-            id: 3,
-            name: 'Moose',
-            hp: 91,
-            maxHp: 91,
-            attacks: [{
-                attackPower: 10,
-                accuracy: 1,
-                name: 'Tackle',
-                simplePast: 'tackled',
-                pastParticiple: 'tackled'
-            }, {
-                attackPower: 20,
-                accuracy: 0.6,
-                name: 'Scratch',
-                simplePast: 'scratched',
-                pastParticiple: 'scratched'
-            }]
-        }];
+        $scope.animals = [];
+
+        $scope.wildAnimals = [];
 
         $scope.startBattle = function() {
+            angular.forEach($scope.animalsFromJson._Species, function(jsonAnimal, index) {
+                var animalToAdd = {
+                    id: jsonAnimal.TaxonID,
+                    name: jsonAnimal.AcceptedCommonName,
+                    hp: 91,
+                    maxHp: 91,
+                    image: jsonAnimal._SpeciesProfile.Image[0].URL,
+                    attacks: $scope.animalClassAttacks[jsonAnimal.ClassName]
+                };
+                if (index % 2 === 1) {
+                    $scope.wildAnimals.push(animalToAdd);
+                } else {
+                    $scope.animals.push(animalToAdd);
+                }
+                $scope.wildAnimals.push();
+            });
             $scope.isNotPlayersTurn = false;
             $scope.getNewActiveAnimal();
             $scope.getNewActiveWildAnimal();
         };
-
 
         $scope.getNewActiveAnimal = function() {
             var activeAnimalIndex = Math.floor(Math.random() * ($scope.animals.length));
@@ -112,14 +83,13 @@ angular.module('nickApp')
                 $scope.isNotPlayersTurn = true;
                 $timeout(function() {
                     $scope.wildAttack(otherAnimal, animal);
-                }, 1000 + 3000 * Math.random());
+                }, 1000 + 2000 * Math.random());
             } else {
                 $scope.battleMessage = otherAnimal.name + ' fainted!';
                 $timeout(function() {
                     $scope.getNewActiveWildAnimal();
                 }, 1000 + 3000 * Math.random());
             }
-
         };
 
         $scope.wildAttack = function(animal, otherAnimal) {
@@ -152,6 +122,172 @@ angular.module('nickApp')
             return animal.hp > 0;
         };
 
+        $scope.animalsFromJson = {
+            "_Species": [{
+                "AcceptedCommonName": "Eastern blue groper",
+                "ClassCommonName": "ray-finned fishes",
+                "ClassName": "Actinopterygii",
+                "ConservationStatus": {
+                    "BOTStatusCode": "C",
+                    "ConservationSignificant": false
+                },
+                "FamilyCommonName": "wrasses",
+                "FamilyName": "Labridae",
+                "FamilyRank": 950770,
+                "IsSuperseded": false,
+                "KingdomCommonName": "animals",
+                "KingdomName": "Animalia",
+                "ScientificName": "Achoerodus viridis",
+                "SpeciesProfileUrl": "http://environment.ehp.qld.gov.au/species/?op=getspeciesbyid&taxonid=34168",
+                "TaxonID": 34168,
+                "_SpeciesProfile": {
+                    "Image": [{
+                        "Format": "jpg",
+                        "Reference": "Richard Ling,2004",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/14876"
+                    }]
+                },
+            }, {
+                "AcceptedCommonName": "Murray cod",
+                "ClassCommonName": "ray-finned fishes",
+                "ClassName": "Actinopterygii",
+                "ConservationStatus": {
+                    "BOTStatusCode": "C",
+                    "ConservationSignificant": true,
+                    "EPBCStatusCode": "V"
+                },
+                "FamilyCommonName": "basses and cods",
+                "FamilyName": "Percichthyidae",
+                "FamilyRank": 950598,
+                "IsSuperseded": false,
+                "KingdomCommonName": "animals",
+                "KingdomName": "Animalia",
+                "ScientificName": "Maccullochella peelii",
+                "SpeciesProfileUrl": "http://environment.ehp.qld.gov.au/species/?op=getspeciesbyid&taxonid=27041",
+                "TaxonID": 27041,
+                "_SpeciesProfile": {
+                    "Image": [{
+                        "Format": "jpg",
+                        "Reference": "Limpus, C.,Department of Environment and Resource Management (DERM),2004",
+                        "Type": "Preferred Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/7010"
+                    }],
+                    "WetlandStatus": "D"
+                },
+
+            }, {
+                "AcceptedCommonName": "cane toad",
+                "ClassCommonName": "amphibians",
+                "ClassName": "Amphibia",
+                "ConservationStatus": {
+                    "ConservationSignificant": false
+                },
+                "FamilyCommonName": "true toads",
+                "FamilyName": "Bufonidae",
+                "FamilyRank": 960040,
+                "IsSuperseded": false,
+                "KingdomCommonName": "animals",
+                "KingdomName": "Animalia",
+                "ScientificName": "Rhinella marina",
+                "SpeciesProfileUrl": "http://environment.ehp.qld.gov.au/species/?op=getspeciesbyid&taxonid=716",
+                "TaxonID": 716,
+                "_SpeciesProfile": {
+                    "Image": [{
+                        "Format": "jpg",
+                        "Reference": "Queensland Government",
+                        "Type": "Preferred Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/4220"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/4221"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "McDonald, K.,Queensland Government,1988",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/4222"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government,1995",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/4223"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government,1994",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/4224"
+                    }]
+                },
+            }, {
+                "AcceptedCommonName": "greenstripe frog",
+                "ClassCommonName": "amphibians",
+                "ClassName": "Amphibia",
+                "ConservationStatus": {
+                    "BOTStatusCode": "L",
+                    "ConservationSignificant": false,
+                    "NCAStatusCode": "C"
+                },
+                "FamilyCommonName": "tree frogs",
+                "FamilyName": "Hylidae",
+                "FamilyRank": 960010,
+                "IsSuperseded": false,
+                "KingdomCommonName": "animals",
+                "KingdomName": "Animalia",
+                "ScientificName": "Cyclorana alboguttata",
+                "SpeciesProfileUrl": "http://environment.ehp.qld.gov.au/species/?op=getspeciesbyid&taxonid=624",
+                "TaxonID": 624,
+                "_SpeciesProfile": {
+                    "Image": [{
+                        "Format": "jpg",
+                        "Reference": "Dollery, C.,DERM,1995",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/522"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Dollery, C.,DERM,2000",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/1323"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government,1978",
+                        "Type": "Preferred Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3776"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government,1978",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3777"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government,1978",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3778"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Hines, H.,Queensland Government,1999",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3779"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Hines, H.,Queensland Government,1999",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3780"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Hines, H.,Queensland Government,1999",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3781"
+                    }, {
+                        "Format": "jpg",
+                        "Reference": "Queensland Government,1978",
+                        "Type": "Available Image",
+                        "URL": "http://wildnet.derm.qld.gov.au/wws/images/3796"
+                    }]
+                },
+            }]
+        };
         $scope.startBattle();
 
         // $interval(function() {
