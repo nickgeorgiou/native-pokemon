98bb940045d558f05843997b0e8252e3d519e6d9
Nick Georgiou <nick@Nicks-MacBook-Air.local>
2014-07-13 07:06:17 +1000

    Latest nick hackery.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,26 +14,186 @@ angular.module('nickApp')
                 accuracy: 0.95,
                 name: 'Croak',
                 simplePast: 'croaked at',
-                pastParticiple: 'croaked at'
+                pastParticiple: 'croaked at',
+                description: 'Croak loudly at the opponent, making them startled.',
+                animation: 'wobble-top',
+                fail: 'failed'
             }, {
                 attackPower: 18,
                 accuracy: 0.65,
-                name: 'Tackled',
+                name: 'Tackle',
                 simplePast: 'tackled',
-                pastParticiple: 'tackled'
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
             }],
             'Actinopterygii': [{
                 attackPower: 10,
                 accuracy: 0.95,
                 name: 'Splash',
-                simplePast: 'splashed toward',
-                pastParticiple: 'splashed at'
+                simplePast: 'splashed',
+                pastParticiple: 'splashed',
+                description: 'A high accuracy attack. Splashes water on the opponent, making them slip over and hurt themselves.',
+                animation: 'bounce',
+                fail: 'missed'
             }, {
                 attackPower: 20,
                 accuracy: 0.6,
                 name: 'Fin Spike',
                 simplePast: 'used fin spike on',
-                pastParticiple: 'fin spiked'
+                pastParticiple: 'fin spiked',
+                description: 'A strong, less accurate attack. Uses this fish\'s spikes to pieces the opponent.',
+                animation: 'bounceInRight',
+                fail: 'missed'
+            }],
+            'Arachnida': [{
+                attackPower: 30,
+                accuracy: 0.55,
+                name: 'Bite',
+                simplePast: 'bit',
+                pastParticiple: 'bitten',
+                description: 'A high accuracy attack. Splashes water on the opponent, making them slip over and hurt themselves.',
+                animation: 'bounce',
+                fail: 'missed'
+            }, {
+                attackPower: 30,
+                accuracy: 0.55,
+                name: 'Bite',
+                simplePast: 'bit',
+                pastParticiple: 'bitten',
+                description: 'A high accuracy attack. Splashes water on the opponent, making them slip over and hurt themselves.',
+                animation: 'wobble-top',
+                fail: 'missed'
+            }],
+            'Aves': [{
+                attackPower: 30,
+                accuracy: 0.55,
+                name: 'Peck',
+                simplePast: 'pecked',
+                pastParticiple: 'pecked',
+                description: 'A high accuracy attack. Splashes water on the opponent, making them slip over and hurt themselves.',
+                animation: 'wobble-top',
+                fail: 'missed'
+            }, {
+                attackPower: 30,
+                accuracy: 0.55,
+                name: 'Claw',
+                simplePast: 'clawed',
+                pastParticiple: 'clawed',
+                description: 'An attack that pierces the opponent with the bird\'s sharp claws.',
+                animation: 'wobble-top',
+                fail: 'missed'
+            }],
+            'Bivalvia': [{
+                attackPower: 30,
+                accuracy: 0.55,
+                name: 'Spike',
+                simplePast: 'spiked',
+                pastParticiple: 'spiked',
+                description: 'Pierce the opponent with this animal\'s sharp spikes',
+                animation: 'bounceInRight',
+                fail: 'missed'
+            }],
+            'Branchiopoda': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Cephalaspidomorphi': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Chondrichthyes': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Gastropoda': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Incertae sedis': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Malacostraca': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Mammalia': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Maxillopoda': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Reptilia': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
+            }],
+            'Sarcopterygii': [{
+                attackPower: 18,
+                accuracy: 0.65,
+                name: 'Tackle',
+                simplePast: 'tackled',
+                pastParticiple: 'tackled',
+                description: 'A strong tackle with a medium level of accuracy.',
+                animation: 'bounceInLeft',
+                fail: 'missed'
             }]
         };
 
@@ -41,7 +201,9 @@ angular.module('nickApp')
 
         $scope.wildAnimals = [];
 
-        $scope.startBattle = function() {
+        $scope.capturedAnimals = [];
+
+        $scope.startGame = function() {
             angular.forEach($scope.animalsFromJson._Species, function(jsonAnimal, index) {
                 var animalToAdd = {
                     id: jsonAnimal.TaxonID,
@@ -49,12 +211,15 @@ angular.module('nickApp')
                     hp: 91,
                     maxHp: 91,
                     image: jsonAnimal._SpeciesProfile.Image[0].URL,
-                    attacks: $scope.animalClassAttacks[jsonAnimal.ClassName]
+                    attacks: $scope.animalClassAttacks[jsonAnimal.ClassName],
+                    className: jsonAnimal.ClassName,
+                    animation: ''
                 };
                 if (index % 2 === 1) {
                     $scope.wildAnimals.push(animalToAdd);
                 } else {
                     $scope.animals.push(animalToAdd);
+                    $scope.capturedAnimals.push(animalToAdd);
                 }
                 $scope.wildAnimals.push();
             });
@@ -63,9 +228,18 @@ angular.module('nickApp')
             $scope.getNewActiveWildAnimal();
         };
 
+        $scope.loseGame = function() {
+            $scope.battleText = 'Game over! You ran out of animals. You managed to capture data on ' + $scope.capturedAnimals.length + 'Queensland animals!';
+        };
+
+        $scope.winGame = function() {
+            $scope.battleText = 'Congratulations! You managed to capture data on all known Queensland animals! You win!';
+        };
+
         $scope.getNewActiveAnimal = function() {
             var activeAnimalIndex = Math.floor(Math.random() * ($scope.animals.length));
             $scope.activeAnimal = $scope.animals[activeAnimalIndex];
+            $scope.battleText = $scope.activeAnimal.name + ' came out!';
             $scope.animals.splice(activeAnimalIndex, 1);
         };
 
@@ -78,32 +252,81 @@ angular.module('nickApp')
 
         $scope.playerAttack = function(animal, otherAnimal, attack) {
             $scope.attackWithAnimal(animal, otherAnimal, attack);
-            console.log($scope.isNotPlayersTurn);
+
             if ($scope.checkAnimalAlive(otherAnimal)) {
                 $scope.isNotPlayersTurn = true;
                 $timeout(function() {
                     $scope.wildAttack(otherAnimal, animal);
-                }, 1000 + 2000 * Math.random());
+                }, 2000);
             } else {
-                $scope.battleMessage = otherAnimal.name + ' fainted!';
-                $timeout(function() {
-                    $scope.getNewActiveWildAnimal();
-                }, 1000 + 3000 * Math.random());
+                if ($scope.wildAnimals.length === 0) {
+                    $scope.battleMessage = otherAnimal.name + ' fainted!';
+                    $timeout(function() {
+                        $scope.battleMessage = otherAnimal.name + '\'s information was added to your collection!';
+                        $timeout(function() {
+                            $scope.winGame();
+                        }, 1000);
+                    }, 2000);
+                } else {
+                    $timeout(function() {
+                        $scope.battleMessage = otherAnimal.name + ' fainted!';
+                    }, 2000);
+                    $timeout(function() {
+                        otherAnimal.hp = otherAnimal.maxHp;
+                        $scope.battleMessage = otherAnimal.name + '\'s information was added to your collection!';
+                        $scope.animals.push(otherAnimal);
+                        $scope.capturedAnimals.push(otherAnimal);
+                    }, 4000);
+                    $timeout(function() {
+                        $scope.getNewActiveWildAnimal();
+                    }, 6000);
+                }
             }
         };
 
         $scope.wildAttack = function(animal, otherAnimal) {
             $scope.attackToUse = animal.attacks[Math.floor(Math.random() * (animal.attacks.length))];
             $scope.attackWithAnimal(animal, otherAnimal, $scope.attackToUse);
+            if ($scope.checkAnimalAlive(otherAnimal)) {
+                $scope.isNotPlayersTurn = false;
+            } else {
+                $timeout(function() {
+                    $scope.battleMessage = otherAnimal.name + ' fainted!';
+                    $timeout(function() {
+                        if ($scope.animals.length === 0) {
+                            $scope.loseGame();
+                        } else {
+                            $scope.getNewActiveAnimal();
+                            $scope.isNotPlayersTurn = false;
+                        }
 
-            $scope.isNotPlayersTurn = false;
+                    }, 2000);
+                }, 2000);
+            }
+            //$scope.isNotPlayersTurn = false;
+        };
 
+        $scope.shakeAnimal = function(animal) {
+            animal.animation = 'shake';
+            $timeout(function() {
+                $scope.stopAnimatingAnimal(animal);
+            }, 800);
+        };
 
+        $scope.stopAnimatingAnimal = function(animal) {
+            animal.animation = '';
         };
 
         $scope.attackWithAnimal = function(animal, otherAnimal, attack) {
+            animal.animation = attack.animation;
+            $timeout(function() {
+                animal.animation = '';
+            }, 1000);
 
             if (Math.random() < attack.accuracy) {
+                $timeout(function() {
+                    $scope.shakeAnimal(otherAnimal);
+                }, 800);
                 $scope.battleMessage = animal.name + ' ' + attack.simplePast + ' ' + otherAnimal.name;
                 if (otherAnimal.hp - attack.attackPower > 0) {
                     otherAnimal.hp -= attack.attackPower;
@@ -111,7 +334,8 @@ angular.module('nickApp')
                     otherAnimal.hp = 0;
                 }
             } else {
-                $scope.battleMessage = animal.name + ' missed!';
+
+                $scope.battleMessage = animal.name + ' tried to ' + attack.name + ' and ' + attack.fail + '!';
             }
             //var otherAnimalHealthbar = _.find(otherAnimal.children('.progress-bar'));
             //otherAnimalHealthbar.removeClass('progress-bar-success');
@@ -288,7 +512,7 @@ angular.module('nickApp')
                 },
             }]
         };
-        $scope.startBattle();
+        $scope.startGame();
 
         // $interval(function() {
         //     var animalIndex = Math.round(Math.random());
